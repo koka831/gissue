5719b3d620320c24fc93cd476b74bad20e8e28b5
koka <koka.code@gmail.com>
2017-04-20 18:00:09 +0900

    init

---
+++ lib/gissue.ex
@@ -0,0 +1,18 @@
+defmodule Gissue do
+  @moduledoc """
+  Documentation for Gissue.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gissue.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gissue_test.exs
@@ -0,0 +1,8 @@
+defmodule GissueTest do
+  use ExUnit.Case
+  doctest Gissue
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
